8debd8449339636bbc94769c960ca321919272f5
ssani7 <sanaullah.sani756@gmail.com>
2024-03-15 00:59:25 +0600

    skill animation optimization

--- src/components/AnimatedSkills.tsx
+++ src/components/AnimatedSkills.tsx
@@ -1,113 +1,37 @@
 import { useState } from 'react';
-import tail from '../assets/icons/Tailwind_CSS_Logo.svg.png';
-import bootstrap from '../assets/icons/bootstrap-logo.png';
-import daisyUi from '../assets/icons/daisyUi.svg';
 import { motion } from 'framer-motion';
 import '../styles/Animation.css';
+import { skills } from '../utils/skills';
 
 const AnimatedSkills = () => {
-	const skills = [
-		[
-			{
-				name: 'HTML',
-				image: 'https://res.cloudinary.com/ssani7/image/upload/v1687032575/potfolio/html_t2yyvm.png',
-			},
-		],
-		[
-			{
-				name: 'ReactJs',
-				image: 'https://res.cloudinary.com/ssani7/image/upload/v1687032575/potfolio/React-icon.svg_k80qir.png',
-			},
-			{
-				name: 'Next.JS',
-				image: 'https://res.cloudinary.com/ssani7/image/upload/v1687032520/potfolio/next-js-icon-512x512-zuauazrk_htfagw.png',
-			},
-			{
-				name: 'Redux',
-				image: 'https://res.cloudinary.com/ssani7/image/upload/v1687036554/potfolio/redux_tqxe3o.svg',
-			},
-		],
-
-		[
-			{
-				name: 'Javascript',
-				image: 'https://res.cloudinary.com/ssani7/image/upload/v1687032575/potfolio/javascript_yvc2kq.png',
-			},
-			{
-				name: 'Node.js',
-				image: 'https://res.cloudinary.com/ssani7/image/upload/v1687032575/potfolio/node_kxkkxz.png',
-			},
-			{
-				name: 'Express',
-				image: 'https://res.cloudinary.com/ssani7/image/upload/v1710432931/potfolio/expressjs.png',
-			},
-			{
-				name: 'MongoDB',
-				image: 'https://res.cloudinary.com/ssani7/image/upload/v1687032575/potfolio/mongo_pfsmxl.png',
-			},
-			{
-				name: 'Typescript',
-				image: 'https://res.cloudinary.com/ssani7/image/upload/v1710433799/potfolio/typescript.png',
-			},
-			{
-				name: 'PostgreSQL',
-				image: 'https://res.cloudinary.com/ssani7/image/upload/v1687033522/potfolio/Postgresql_elephant.svg_x7jy3o.png',
-			},
-		],
-		[
-			{
-				name: 'Material UI',
-				image: 'https://res.cloudinary.com/ssani7/image/upload/v1687032519/potfolio/mui_jjgtsm.png',
-			},
-			{
-				name: 'Tailwind CSS',
-				image: tail,
-			},
-			{
-				name: 'Bootstrap',
-				image: bootstrap,
-			},
-			{
-				name: 'DaisyUI',
-				image: daisyUi,
-			},
-			{
-				name: 'Git Hub',
-				image: 'https://res.cloudinary.com/ssani7/image/upload/v1687032576/potfolio/25231_l7njk3.png',
-			},
-			{
-				name: 'Firebase',
-				image: 'https://res.cloudinary.com/ssani7/image/upload/v1687032575/potfolio/firebase_aj8k3h.png',
-			},
-		],
-	];
-
 	const [focus, setFocus] = useState<any>(null);
 
 	return (
 		<div className="pb-32 w-full overflow-x-hidden snap-center min-h-screen relative">
 			<h1 className="mt-8 mb-28 text-center font-bold text-3xl">Skills</h1>
 
-			{<motion.div initial={{ opacity: 0 }} animate={focus && { opacity: 0.35 }} transition={{ duration: 0.4 }} className="bg-black absolute inset-0 z-[10] h-[100vh]"></motion.div>}
+			{<motion.div initial={{ opacity: 0 }} animate={focus && { opacity: 0.35 }} transition={{ duration: 0.4 }} className="bg-black absolute inset-0 z-[10] min-h-[100vh]"></motion.div>}
 
 			<div className="relative">
-				<motion.div initial={{ x: '-100%' }} animate={focus && { x: 18 }} className="stack absolute left-0 top-[40%] z-10">
-					<div className="card shadow-md bg-base-200 text-black">
-						<div className="card-body">
-							<h2 className="card-title">{focus?.name}</h2>
-							<p>You have 3 unread messages. Tap here to see.</p>
+				<motion.div initial={{ x: '-100%' }} animate={focus && { x: 18 }} className="absolute left-0 top-0 bottom-0 flex flex-col justify-center items-center z-10 w-80">
+					<div className="stack">
+						<div className="card h-full shadow-md bg-base-200 text-black">
+							<div className="card-body">
+								<h2 className="card-title">{focus?.name}</h2>
+								<p>{focus?.desc}</p>
+							</div>
 						</div>
-					</div>
-					<div className="card shadow bg-base-300 text-black">
-						<div className="card-body">
-							<h2 className="card-title">Notification 2</h2>
-							<p>You have 3 unread messages. Tap here to see.</p>
+						<div className="card h-full shadow bg-base-300 text-black">
+							<div className="card-body">
+								<h2 className="card-title">Notification 2</h2>
+								<p>You have 3 unread messages. Tap here to see.</p>
+							</div>
 						</div>
-					</div>
-					<div className="card shadow-sm bg-accent text-black">
-						<div className="card-body">
-							<h2 className="card-title">Notification 3</h2>
-							<p>You have 3 unread messages. Tap here to see.</p>
+						<div className="card h-full shadow-sm bg-accent text-black">
+							<div className="card-body">
+								<h2 className="card-title">Notification 3</h2>
+								<p>You have 3 unread messages. Tap here to see.</p>
+							</div>
 						</div>
 					</div>
 				</motion.div>
---
+++ src/utils/skills.tsx
@@ -0,0 +1,95 @@
+import tail from '../assets/icons/Tailwind_CSS_Logo.svg.png';
+import bootstrap from '../assets/icons/bootstrap-logo.png';
+import daisyUi from '../assets/icons/daisyUi.svg';
+
+export const skills = [
+	[
+		{
+			name: 'HTML',
+			image: 'https://res.cloudinary.com/ssani7/image/upload/v1687032575/potfolio/html_t2yyvm.png',
+			desc: 'HTML (Hypertext Markup Language) is the standard markup language for creating web pages. It defines the structure and content of a webpage using tags and elements.',
+		},
+	],
+	[
+		{
+			name: 'ReactJs',
+			image: 'https://res.cloudinary.com/ssani7/image/upload/v1687032575/potfolio/React-icon.svg_k80qir.png',
+			desc: 'React.js is a popular JavaScript library for building user interfaces. It allows developers to create reusable components and efficiently manage state changes in web applications.',
+		},
+		{
+			name: 'Next.JS',
+			image: 'https://res.cloudinary.com/ssani7/image/upload/v1687032520/potfolio/next-js-icon-512x512-zuauazrk_htfagw.png',
+			desc: 'Next.js is the most used React framework for building full-stack web applications. It provides features like server-side rendering, automatic routing, and easy setup, making it ideal for React projects.',
+		},
+		{
+			name: 'Redux',
+			image: 'https://res.cloudinary.com/ssani7/image/upload/v1687036554/potfolio/redux_tqxe3o.svg',
+			desc: 'Redux is a predictable state container for JavaScript apps. It helps manage application state in a single place, making changes more predictable and traceable.',
+		},
+	],
+
+	[
+		{
+			name: 'Javascript',
+			image: 'https://res.cloudinary.com/ssani7/image/upload/v1687032575/potfolio/javascript_yvc2kq.png',
+			desc: 'JavaScript is the most popular and versatile programming language used for web development. It runs in browsers and on servers, enabling dynamic and interactive web experiences.',
+		},
+		{
+			name: 'Node.js',
+			image: 'https://res.cloudinary.com/ssani7/image/upload/v1687032575/potfolio/node_kxkkxz.png',
+			desc: 'Node.js is a server-side JavaScript runtime environment. It allows developers to build scalable and efficient network applications using JavaScript.',
+		},
+		{
+			name: 'Express',
+			image: 'https://res.cloudinary.com/ssani7/image/upload/v1710432931/potfolio/expressjs.png',
+			desc: 'Express.js is a minimal and flexible Node.js web application framework. It simplifies server-side development by providing routing, middleware, and other utilities.',
+		},
+		{
+			name: 'MongoDB',
+			image: 'https://res.cloudinary.com/ssani7/image/upload/v1687032575/potfolio/mongo_pfsmxl.png',
+			desc: 'MongoDB is a Non sequel document database designed for modern application development and the cloud. It uses a scale-out architecture, allowing you to add more nodes to handle increasing demand.',
+		},
+		{
+			name: 'Typescript',
+			image: 'https://res.cloudinary.com/ssani7/image/upload/v1710433799/potfolio/typescript.png',
+			desc: 'TypeScript is a superset of JavaScript that adds static typing. It enhances code quality, improves tooling, and provides better developer experience.',
+		},
+		{
+			name: 'PostgreSQL',
+			image: 'https://res.cloudinary.com/ssani7/image/upload/v1687033522/potfolio/Postgresql_elephant.svg_x7jy3o.png',
+			desc: 'PostgreSQL is a powerful open-source relational database management system (RDBMS). It supports advanced features like window functions, common table expressions, and full-text search. PostgreSQL is known for its robustness, extensibility, and ACID compliance',
+		},
+	],
+	[
+		{
+			name: 'Material UI',
+			image: 'https://res.cloudinary.com/ssani7/image/upload/v1687032519/potfolio/mui_jjgtsm.png',
+			desc: 'Material UI implements Google’s Material Design principles in React components. It combines aesthetics with functionality and provides a rich set of UI elements for web development',
+		},
+		{
+			name: 'Tailwind CSS',
+			image: tail,
+			desc: 'Tailwind CSS is a utility-first CSS framework that allows developers to build custom designs using utility classes. It emphasizes simplicity, speed, and responsive design',
+		},
+		{
+			name: 'Bootstrap',
+			image: bootstrap,
+			desc: 'Bootstrap is a popular front-end framework that provides pre-defined components and styling for web applications.',
+		},
+		{
+			name: 'DaisyUI',
+			image: daisyUi,
+			desc: 'Daisy UI extends Tailwind CSS by adding customizable color names and class names for common UI components. It simplifies styling and enhances the developer experience',
+		},
+		{
+			name: 'Git Hub',
+			image: 'https://res.cloudinary.com/ssani7/image/upload/v1687032576/potfolio/25231_l7njk3.png',
+			desc: 'GitHub is a platform where over 100 million developers collaborate on software projects. It allows version control, code review, issue tracking, and continuous integration',
+		},
+		{
+			name: 'Firebase',
+			image: 'https://res.cloudinary.com/ssani7/image/upload/v1687032575/potfolio/firebase_aj8k3h.png',
+			desc: 'Firebase is a comprehensive platform by Google for building web and mobile applications. It provides services like authentication, real-time databases, hosting, and cloud functions',
+		},
+	],
+];
